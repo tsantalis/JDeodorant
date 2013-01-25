652473df9f782591e80b35a7318965564dcaafd1
tsantalis <tsantalis@gmail.com>
2013-01-25 21:35:07 +0000

    Updated LiteralObject so that it stores the actual type of the literal. For example, a NumberLiteral may be an int, double, etc.
    git-svn-id: svn://java.uom.gr/jdeodorant/CloneRefactoring@417 b7e68eb2-82c8-4b8b-a2e2-35572869c0fb

--- src/gr/uom/java/ast/LiteralObject.java
+++ src/gr/uom/java/ast/LiteralObject.java
@@ -9,70 +9,82 @@ import org.eclipse.jdt.core.dom.StringLiteral;
 import org.eclipse.jdt.core.dom.TypeLiteral;
 
 public class LiteralObject {
-	private LiteralType type;
+	private LiteralType literalType;
 	private String value;
+	private TypeObject type;
 	private ASTInformation literal;
 	private volatile int hashCode = 0;
 	
 	public LiteralObject(Expression expression) {
 		if(expression instanceof StringLiteral) {
 			StringLiteral stringLiteral = (StringLiteral)expression;
-			type = LiteralType.STRING;
+			literalType = LiteralType.STRING;
 			value = stringLiteral.getLiteralValue();
+			type = TypeObject.extractTypeObject(stringLiteral.resolveTypeBinding().getQualifiedName());
 		}
 		else if(expression instanceof NullLiteral) {
-			type = LiteralType.NULL;
+			NullLiteral nullLiteral = (NullLiteral)expression;
+			literalType = LiteralType.NULL;
 			value = "null";
+			type = TypeObject.extractTypeObject(nullLiteral.resolveTypeBinding().getQualifiedName());
 		}
 		else if(expression instanceof NumberLiteral) {
 			NumberLiteral numberLiteral = (NumberLiteral)expression;
-			type = LiteralType.NUMBER;
+			literalType = LiteralType.NUMBER;
 			value = numberLiteral.getToken();
+			type = TypeObject.extractTypeObject(numberLiteral.resolveTypeBinding().getQualifiedName());
 		}
 		else if(expression instanceof BooleanLiteral) {
 			BooleanLiteral booleanLiteral = (BooleanLiteral)expression;
-			type = LiteralType.BOOLEAN;
+			literalType = LiteralType.BOOLEAN;
 			value = Boolean.toString(booleanLiteral.booleanValue());
+			type = TypeObject.extractTypeObject(booleanLiteral.resolveTypeBinding().getQualifiedName());
 		}
 		else if(expression instanceof CharacterLiteral) {
 			CharacterLiteral characterLiteral = (CharacterLiteral)expression;
-			type = LiteralType.CHARACTER;
+			literalType = LiteralType.CHARACTER;
 			value = Character.toString(characterLiteral.charValue());
+			type = TypeObject.extractTypeObject(characterLiteral.resolveTypeBinding().getQualifiedName());
 		}
 		else if(expression instanceof TypeLiteral) {
 			TypeLiteral typeLiteral = (TypeLiteral)expression;
-			type = LiteralType.TYPE;
+			literalType = LiteralType.TYPE;
 			value = typeLiteral.getType().toString();
+			type = TypeObject.extractTypeObject(typeLiteral.resolveTypeBinding().getQualifiedName());
 		}
 		this.literal = ASTInformationGenerator.generateASTInformation(expression);
 	}
 
-	public LiteralType getType() {
-		return type;
+	public LiteralType getLiteralType() {
+		return literalType;
 	}
 
 	public String getValue() {
 		return value;
 	}
-	
+
+	public TypeObject getType() {
+		return type;
+	}
+
 	public Expression getLiteral() {
 		Expression expression = null;
-		if(type.equals(LiteralType.BOOLEAN)) {
+		if(literalType.equals(LiteralType.BOOLEAN)) {
 			expression = (BooleanLiteral)literal.recoverASTNode();
 		}
-		else if(type.equals(LiteralType.CHARACTER)) {
+		else if(literalType.equals(LiteralType.CHARACTER)) {
 			expression = (CharacterLiteral)literal.recoverASTNode();
 		}
-		else if(type.equals(LiteralType.NULL)) {
+		else if(literalType.equals(LiteralType.NULL)) {
 			expression = (NullLiteral)literal.recoverASTNode();
 		}
-		else if(type.equals(LiteralType.NUMBER)) {
+		else if(literalType.equals(LiteralType.NUMBER)) {
 			expression = (NumberLiteral)literal.recoverASTNode();
 		}
-		else if(type.equals(LiteralType.STRING)) {
+		else if(literalType.equals(LiteralType.STRING)) {
 			expression = (StringLiteral)literal.recoverASTNode();
 		}
-		else if(type.equals(LiteralType.TYPE)) {
+		else if(literalType.equals(LiteralType.TYPE)) {
 			expression = (TypeLiteral)literal.recoverASTNode();
 		}
 		return expression;
@@ -85,7 +97,7 @@ public class LiteralObject {
 
         if (o instanceof LiteralObject) {
         	LiteralObject literalObject = (LiteralObject)o;
-            return this.type.equals(literalObject.type) && this.value.equals(literalObject.value);
+            return this.literalType.equals(literalObject.literalType) && this.value.equals(literalObject.value);
         }
         
         return false;
@@ -95,7 +107,7 @@ public class LiteralObject {
 		if(hashCode == 0) {
 			int result = 17;
 			result = 37*result + value.hashCode();
-			result = 37*result + type.hashCode();
+			result = 37*result + literalType.hashCode();
 			hashCode = result;
 		}
 		return hashCode;

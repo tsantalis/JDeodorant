682c051e3fa6dcc530f1e3b56996c8c28613e15b
tsantalis <tsantalis@gmail.com>
2015-08-11 12:44:59 -0400

    Extended ASTNodeMatcher to reduce the weight of TYPE_COMPATIBLE_REPLACEMENT differences that are reversed from previous differences appearing in the arguments of method calls and class instance creations.

--- src/gr/uom/java/ast/decomposition/matching/BindingSignaturePair.java
+++ src/gr/uom/java/ast/decomposition/matching/BindingSignaturePair.java
@@ -39,6 +39,13 @@ public class BindingSignaturePair {
 		this.isGap = isGap;
 	}
 
+	public boolean isReverse(BindingSignaturePair other) {
+		if(this.signature1.signatureWithoutMethods().equals(other.signature2.signatureWithoutMethods()) &&
+				this.signature2.signatureWithoutMethods().equals(other.signature1.signatureWithoutMethods()))
+			return true;
+		return false;
+	}
+
 	public int getWeight() {
 		return signature1.getSize() + signature2.getSize();
 	}
